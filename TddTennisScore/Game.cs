f11176fd9d900f33a3f6dbfafff3ff958165b35a
urbrioche <aafulin@gmail.com>
2018-04-21 15:40:27 +0800

    create basic class

---
+++ TddTennisScore/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TddTennisScore
+{
+    public class Game
+    {
+        public int FirstPlayerScore { get; set; }
+        public int SecondPlayerScore { get; set; }
+        public int Id { get; set; }  
+    }
+
+    public class TennisGame
+    {
+        private readonly IRepository<Game> _repo;
+
+        public TennisGame(IRepository<Game> repo)
+        {
+            _repo = repo;
+        }
+
+        public string ScoreResult(int gameId)
+        {
+            var game = _repo.GetGame(gameId);
+
+            throw new NotImplementedException();
+        }
+    }
+}
