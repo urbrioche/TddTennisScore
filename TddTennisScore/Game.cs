6eb03d25024450b019a0870d16da417173b13243
urbrioche <aafulin@gmail.com>
2018-04-21 16:48:14 +0800

    finish

--- TddTennisScore/Game.cs
+++ TddTennisScore/Game.cs
@@ -1,28 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TddTennisScore
 {
     public class Game
     {
+        private const string Deuce = "Deuce";
+
+        private readonly Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            [0] = "Love",
+            [1] = "Fifteen",
+            [2] = "Thirty",
+            [3] = "Forty"
+        };
+
         public int FirstPlayerScore { get; set; }
         public int SecondPlayerScore { get; set; }
-        public int Id { get; set; }  
-    }
+        public int Id { get; set; }
+        public string FirstPlayerName { get; set; }
+        public string SecondPlayerName { get; set; }
 
-    public class TennisGame
-    {
-        private readonly IRepository<Game> _repo;
-
-        public TennisGame(IRepository<Game> repo)
+        public bool IsDeuce()
         {
-            _repo = repo;
+            return FirstPlayerScore >= 3;
         }
 
-        public string ScoreResult(int gameId)
+        public bool IsSameScore()
         {
-            var game = _repo.GetGame(gameId);
+            return FirstPlayerScore == SecondPlayerScore;
+        }
 
-            throw new NotImplementedException();
+        public string SameScoreLookup()
+        {
+            return $"{_scoreLookup[FirstPlayerScore]} All";
+        }
+
+        public string ScoreResult()
+        {
+            return IsSameScore()
+                ? (IsDeuce() ? Deuce : SameScoreLookup())
+                : (IsReadyForWIn() ? AdvState() : LookupScore());
+        }
+
+        private string AdvState()
+        {
+            return AdvPlayer() + (IsAdv() ? " Adv" : " Win");
+        }
+
+        private string AdvPlayer()
+        {
+            return (FirstPlayerScore > SecondPlayerScore ? FirstPlayerName : SecondPlayerName);
+        }
+
+        private bool IsAdv()
+        {
+            return Math.Abs(FirstPlayerScore - SecondPlayerScore) == 1;
+        }
+
+        private bool IsReadyForWIn()
+        {
+            return FirstPlayerScore > 3 || SecondPlayerScore > 3;
+        }
+
+        private string LookupScore()
+        {
+            return _scoreLookup[FirstPlayerScore] + " " + _scoreLookup[SecondPlayerScore];
         }
     }
 }
---
+++ TddTennisScore/TennisGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TddTennisScore
+{
+    public class TennisGame
+    {
+        private readonly IRepository<Game> _repo;
+
+        public TennisGame(IRepository<Game> repo)
+        {
+            _repo = repo;
+        }
+
+        public string ScoreResult(int gameId)
+        {
+            var game = _repo.GetGame(gameId);
+            return game.ScoreResult();
+        }
+    }
+}
