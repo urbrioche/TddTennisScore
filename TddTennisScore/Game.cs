6d7f38bd88ab67214617130beb053b7fe443a02d
urbrioche <aafulin@gmail.com>
2018-04-25 21:27:54 +0800

    complete

--- TddTennisScore/Game.cs
+++ TddTennisScore/Game.cs
@@ -7,6 +7,65 @@ namespace TddTennisScore
     {
         public int FirstPlayerScore { get; set; }
         public int SecondPlayerScore { get; set; }
-        public int Id { get; set; }        
+        public int Id { get; set; }
+        public string FirstPlayerName { get; set; }
+        public string SecondPlayerName { get; set; }
+
+        public static Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            [0] = "Love",
+            [1] = "Fifteen",
+            [2] = "Thirty",
+            [3] = "Forty"
+         };
+
+        public string SameScoreLookup()
+        {
+            return Game._scoreLookup[FirstPlayerScore] + " All";
+        }
+
+        public bool IsDeuce()
+        {
+            return FirstPlayerScore >= 3;
+        }
+
+        public bool IsSameScore()
+        {
+            return FirstPlayerScore == SecondPlayerScore;
+        }
+
+        public string ScoreResult()
+        {
+            return IsSameScore()
+                ? (IsDeuce() ? Deuce : SameScoreLookup())
+                : (IsReadyForWin() ? AdvState() : LookupScore());
+        }
+
+        private string LookupScore()
+        {
+            return _scoreLookup[FirstPlayerScore] + " " + _scoreLookup[SecondPlayerScore];
+        }
+
+        private string AdvState()
+        {
+            return AdvPlayer() + (IsAdv() ? " Adv" : " Win");
+        }
+
+        private string AdvPlayer()
+        {
+            return (FirstPlayerScore > SecondPlayerScore ? FirstPlayerName : SecondPlayerName);
+        }
+
+        private bool IsAdv()
+        {
+            return Math.Abs(FirstPlayerScore - SecondPlayerScore) == 1;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return FirstPlayerScore > 3 || SecondPlayerScore > 3;
+        }
+
+        private const string Deuce = "Deuce";
     }
 }
